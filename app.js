b19f7c792975c418c7f1372ab60916727bc336f1
skit <skit@skit-MacBook-Air.local>
2025-02-26 00:23:14 +0900

    大幅な改変

--- app.js
+++ app.js
@@ -1,58 +1,70 @@
-// ファイルの読み込み
-fetch(
-  "https://raw.githubusercontent.com/skit-02/P3R_ReverseLookup/main/arcn.txt"
-)
-  .then((response) => response.text()) // テキストとして取得
-  .then((text) => {
-    let lines = text.split("\n");
-    const ARCNs = lines.map((line) => line.split(" "));
-  })
-  .catch((error) => console.error("Error:", error));
-fetch(
-  "https://raw.githubusercontent.com/skit-02/P3R_ReverseLookup/main/prsn.txt"
-)
-  .then((response) => response.text()) // テキストとして取得
-  .then((text) => {
-    let lines = text.split("\n");
-    const PRSNs = lines.map((line) => line.split(" "));
-  })
-  .catch((error) => console.error("Error:", error));
 
 // 素材のペルソナがからの場合は全組み合わせを表示するようにでもするか
 // 作成に必要なペルソナとレベルの組み合わせを計算する
 // PRSNsと照らし合わせて合体相手の(名前とアルカナ)を返す
-// やっぱアルカナの合体表ちょっと変えたほうがいいかもしれない普通にコンビで22C2のやつ
+
 function calc(prsn, sozai, ARCNs, PRSNs) {
   // prsnもsozaiも一次元配列(getPrsnのやつ)
   // ARCNsとPRSNsは二次元配列
-  // 相手のアルカナを探す
-  let ansArcn = ARCNs[sozai[2]].reduce((indexes, value, index) => {
-    if (value === prsn[2]) {
-      indexes.push(index);
+  const errMassage = sozai[0] + "では" + prsn[0] + "は作れません";
+  const filterarcn = ARCNs.filter((row) => row[2] == prsn[2]);
+  let arcnList = [];
+  filterarcn.forEach((row) => {
+    if (row[0] == sozai[2]) arcnList.push(row[1]);
+    else if (row[1] == sozai[2]) arcnList.push(row[0]);
+  });
+  if (arcnList.length === 0) return errMassage;
+  const ans = [];
+  arcnList.forEach((row) => {
+    // 作りたいアルカナのペルソナ一覧を取得
+    const wantPrsn = PRSNs.filter((row2) => row2[2] == prsn[2]);
+    // 使いたい（求めたい）アルカナのペルソナ一覧を取得
+    const usePrsn = PRSNs.filter((row2) => row2[2] == row);
+    // もしsozai == ansArcn ならアルカナ合体時にレベルが下のものを作る
+    if (row == sozai[2]) {
+      ans.push("同じ");
+    } else {
+      ans.push("違う");
     }
-    return indexes;
-  }, []);
-  errMassage = sozai[0] + "では" + prsn[0] + "は作れません";
-  if (ansArcn[0] == -1) return errMassage;
-  // もしsozai == ansArcn ならどうアルカナ合体用にレベルが下のものを作る
+  });
+  return ans;
   // それ以外は足し合わせて上のペルソナを作る(これはアルカナが複数ある場合があるのでansArcn内のでループする)
-  
 }
 // ペルソナのアルカナを返す
 function getPrsn(prsn, PRSNs) {
   // 入力したペルソナの["名前", "レベル", "アルカナ番号", "特殊合体の有無(0,-1)"]を返す
-  let arcn = PRSNs.filter((row) => row.includes(prsn));
-  return arcn;
+  return PRSNs.find((row) => row.includes(prsn));
 }
 
-$("#LookUp").click(function () {
-  let prsn = $("#prsn").val();
-  let sozai = $("#sozai").val();
-  prsn = getPrsn(prsn, PRSNs);
-  sozai = getPrsn(sozai, PRSNs);
-  // calcを呼び出して結果を入れる
-  const ans = calc(prsn, sozai, ARCNs, PRSNs);
-  $("#prsnList").append("<li>" + ans + "</li>");
-  $("#prsn").val("");
-  $("#sozai").val("");
-});
+
+// ファイルの読み込み
+Promise.all([
+  fetch(
+    "https://raw.githubusercontent.com/skit-02/P3R_ReverseLookup/main/arcn.txt"
+  ).then((res) => res.text()),
+  fetch(
+    "https://raw.githubusercontent.com/skit-02/P3R_ReverseLookup/main/prsn.txt"
+  ).then((res) => res.text()),
+])
+  .then(([arcnText, prsnText]) => {
+    const ARCNs = arcnText.split("\n").map((line) => line.split(" "));
+    const PRSNs = prsnText.split("\n").map((line) => line.split(" "));
+
+    $("#LookUp").click(function () {
+      let prsn = $("#prsn").val();
+      let sozai = $("#sozai").val();
+      prsn = getPrsn(prsn, PRSNs);
+      sozai = getPrsn(sozai, PRSNs);
+
+      if (prsn[3] == 0) {
+        console.log(prsn[0] + "は特殊合体です");
+        return;
+      }
+
+      const ans = calc(prsn, sozai, ARCNs, PRSNs);
+      $("#prsnList").append("<li>" + ans + "</li>");
+      $("#prsn").val("");
+      $("#sozai").val("");
+    });
+  })
+  .catch((error) => console.error("Error:", error));
